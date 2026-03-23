c0561d9064f358e732a5a57be8f165f11f6f3bf7
KaykeTorres <kaykeallantorresdutra@gmail.com>
2026-03-23 15:17:48 -0300

    add 6.1 condicionais.js

---
+++ condicionais.js
@@ -0,0 +1,28 @@
+const idade = 17
+
+if (idade >= 18) {
+    console.log ('É maior de idade!')
+} else {
+    console.log ('É menor de idade!')
+}
+
+// if (idade < 18) {
+//    console.log ('É menor de idade!')
+// }
+
+// entre 9 e 10: exelente
+// entre 7 e 8: bom
+// entre 4 e 6: médio
+// entre 3 e 0: ruim
+
+const notaDoAluno = 10
+
+if (notaDoAluno >= 9 ) {
+    console.log ('Nota Exelente')
+} else if (notaDoAluno >= 7) {
+    console.log ('Nota boa')
+} else if (notaDoAluno >= 4) {
+    console.log ('Nota média')
+} else if (notaDoAluno >= 0) {
+    console.log ('Nota ruim')
+}
